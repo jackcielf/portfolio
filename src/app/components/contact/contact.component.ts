147c95f09278c737daf2857cf778265291eae891
JakkiFx <jackcielfelix555@gmail.com>
2023-10-14 12:00:58 -0300

    atualizando animações

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,6 +9,7 @@ export class ContactComponent implements OnInit {
   apiUrl = 'https://api.staticforms.xyz/submit';
   accessKey = '583e11ac-bcca-44d4-be9b-c5a29332aa2f';
   afterSubmitRedirecTo = 'https://portfolio-jakki.netlify.app/contact';
+  myEmail = "jackcielfelix.dev@gmail.com";
 
   constructor() { }
 
