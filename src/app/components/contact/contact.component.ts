2f637b31ff6288d3a80720fd9850da5d0d643775
JakkiFx <jackcielfelix555@gmail.com>
2023-10-30 19:41:37 -0300

    ajustes na responsividade

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -10,7 +10,7 @@ import { fadeAnimation } from 'src/app/shared/animations/animationInput';
 export class ContactComponent implements OnInit {
   apiUrl = 'https://api.staticforms.xyz/submit';
   accessKey = '583e11ac-bcca-44d4-be9b-c5a29332aa2f';
-  afterSubmitRedirecTo = 'https://portfolio-jakki.netlify.app/contact';
+  afterSubmitRedirecTo = 'https://portfolio-jakki.netlify.app/';
   myEmail = "jackcielfelix.dev@gmail.com";
 
   constructor() { }
